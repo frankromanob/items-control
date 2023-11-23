b2398749c3544329653d147c4e6ef18ee39b02c3
Frank Romano <romano.fr@gmail.com>
2023-11-23 17:32:21 -0400

    Products CRUD

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,10 @@
-import { SideBar } from '@/ui/SideBar'
-import styles from '@/app/page.module.css'
+
 import { Typography } from '@mui/material'
 
 export default function Salidas() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main >
+      <div >
         <Typography >Login</Typography>
       </div>
     </main>
