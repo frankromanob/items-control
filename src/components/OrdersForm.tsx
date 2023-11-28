1f73f211eb63a26946c12e28a6f5b65b41664672
Frank Romano <romano.fr@gmail.com>
2023-11-28 17:29:08 -0400

    Entries

--- src/components/OrdersForm.tsx
+++ src/components/OrdersForm.tsx
@@ -112,112 +112,113 @@ export default function EntriesForm({ order }: Props) {
     }
 
     return (
-        !productsLoaded ? <Typography marginInlineStart='5px' fontWeight='bold' color='secondary'>Cargando productos...</Typography>
-            : <Box sx={{ display: 'flex', flexDirection: 'column', }}>
+        <>hola</>
+        // !productsLoaded ? <Typography marginInlineStart='5px' fontWeight='bold' color='secondary'>Cargando productos...</Typography>
+        //     : <Box sx={{ display: 'flex', flexDirection: 'column', }}>
 
-                <form name='entryForm' onSubmit={handleSubmit(onSubmit)}>
-                    <Grid container spacing={2} mt={1}>
-                        <Grid item xs={10} >
-                            <Grid item xs={6} sm={3} mb={2} key={getValues('product')}>
-                                <Card>
-                                    {/* <CardMedia
-                                        component='img'
-                                        className='fadeIn'
-                                        width='200'
-                                        height='100%'
-                                        image={`${getValues('productImage')}`}
-                                        alt={getValues('productName')}
-                                    /> */}
-                                </Card>
-                            </Grid>
-                            <Box sx={{ display: 'flex', flexDirection: 'row', width: '50', height: '30', mb: 1 }}>
-                                <TextField
-                                    select
-                                    fullWidth
-                                    id="Producto"
-                                    defaultValue=''
-                                    label="Producto"
-                                    {...register("product", {
-                                        required: 'Este campo es requerido',
-                                    })}
-                                    error={!!errors.product}
-                                    helperText={errors.product?.message}
-                                    onChange={(event) => onSelectProduct(event.target.value)}
-                                >
-                                    {productList.map((product) => (
-                                        <MenuItem key={product._id} value={product._id} sx={{ display: 'flex', flexDirection: 'row' }} >
-                                            {/* <Card>
-                                                <CardMedia
-                                                    component='img'
-                                                    className='fadeIn'
-                                                    width='20px'
-                                                    height='20px'
-                                                    image={`${product.images[0]}`}
-                                                    alt={product.title}
-                                                />
-                                            </Card> */}
-                                            {product.title}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </Box>
+        //         <form name='entryForm' onSubmit={handleSubmit(onSubmit)}>
+        //             <Grid container spacing={2} mt={1}>
+        //                 <Grid item xs={10} >
+        //                     <Grid item xs={6} sm={3} mb={2} key={getValues('product')}>
+        //                         <Card>
+        //                             {/* <CardMedia
+        //                                 component='img'
+        //                                 className='fadeIn'
+        //                                 width='200'
+        //                                 height='100%'
+        //                                 image={`${getValues('productImage')}`}
+        //                                 alt={getValues('productName')}
+        //                             /> */}
+        //                         </Card>
+        //                     </Grid>
+        //                     <Box sx={{ display: 'flex', flexDirection: 'row', width: '50', height: '30', mb: 1 }}>
+        //                         <TextField
+        //                             select
+        //                             fullWidth
+        //                             id="Producto"
+        //                             defaultValue=''
+        //                             label="Producto"
+        //                             {...register("product", {
+        //                                 required: 'Este campo es requerido',
+        //                             })}
+        //                             error={!!errors.product}
+        //                             helperText={errors.product?.message}
+        //                             onChange={(event) => onSelectProduct(event.target.value)}
+        //                         >
+        //                             {productList.map((product) => (
+        //                                 <MenuItem key={product._id} value={product._id} sx={{ display: 'flex', flexDirection: 'row' }} >
+        //                                     {/* <Card>
+        //                                         <CardMedia
+        //                                             component='img'
+        //                                             className='fadeIn'
+        //                                             width='20px'
+        //                                             height='20px'
+        //                                             image={`${product.images[0]}`}
+        //                                             alt={product.title}
+        //                                         />
+        //                                     </Card> */}
+        //                                     {product.title}
+        //                                 </MenuItem>
+        //                             ))}
+        //                         </TextField>
+        //                     </Box>
 
 
-                            <TextField
-                                label="Cantidad"
-                                variant="outlined"
-                                fullWidth
-                                type='number'
-                                autoComplete='false'
-                                id='quantity'
-                                name='quantity'
-                                sx={{ mb: 1 }}
-                                {...register('quantity', {
-                                    required: 'Este campo es requerido',
-                                    validate: (val) => val < 1 ? 'No puede ser menor a 1' : undefined
-                                })}
-                                error={!!errors.quantity}
-                                helperText={errors.quantity?.message}
-                            />
-                            <TextField
-                                label="Estado"
-                                variant="outlined"
-                                fullWidth
-                                autoComplete='false'
-                                id='status'
-                                name='status'
-                                value={getValues('status')}
-                                sx={{ mb: 1 }}
-                                {...register('status', {})}
-                                error={!!errors.status}
-                            //helperText={errors.email?.message}
-                            />
+        //                     <TextField
+        //                         label="Cantidad"
+        //                         variant="outlined"
+        //                         fullWidth
+        //                         type='number'
+        //                         autoComplete='false'
+        //                         id='quantity'
+        //                         name='quantity'
+        //                         sx={{ mb: 1 }}
+        //                         {...register('quantity', {
+        //                             required: 'Este campo es requerido',
+        //                             validate: (val) => val < 1 ? 'No puede ser menor a 1' : undefined
+        //                         })}
+        //                         error={!!errors.quantity}
+        //                         helperText={errors.quantity?.message}
+        //                     />
+        //                     <TextField
+        //                         label="Estado"
+        //                         variant="outlined"
+        //                         fullWidth
+        //                         autoComplete='false'
+        //                         id='status'
+        //                         name='status'
+        //                         value={getValues('status')}
+        //                         sx={{ mb: 1 }}
+        //                         {...register('status', {})}
+        //                         error={!!errors.status}
+        //                     //helperText={errors.email?.message}
+        //                     />
 
-                            <Divider sx={{ my: 1 }} />
-                            <Box display='flex' flexDirection='row' justifyContent='space-between' sx={{ mb: 1 }}>
-                                <Button
-                                    color="error"
-                                    startIcon={<DeleteOutline />}
-                                    sx={{ width: '100px', height: '30px', marginInlineEnd: '20px' }}
-                                    type="button"
-                                    onClick={() => onDelete(getValues('_id'))}
-                                >
-                                    Eliminar
-                                </Button>
-                                <Button
-                                    color="secondary"
-                                    startIcon={<SaveOutlined />}
-                                    sx={{ width: '150px', height: '30px' }}
-                                    type="submit"
-                                    disabled={isSaving}
-                                >
-                                    Guardar
-                                </Button>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Box >
+        //                     <Divider sx={{ my: 1 }} />
+        //                     <Box display='flex' flexDirection='row' justifyContent='space-between' sx={{ mb: 1 }}>
+        //                         <Button
+        //                             color="error"
+        //                             startIcon={<DeleteOutline />}
+        //                             sx={{ width: '100px', height: '30px', marginInlineEnd: '20px' }}
+        //                             type="button"
+        //                             onClick={() => onDelete(getValues('_id'))}
+        //                         >
+        //                             Eliminar
+        //                         </Button>
+        //                         <Button
+        //                             color="secondary"
+        //                             startIcon={<SaveOutlined />}
+        //                             sx={{ width: '150px', height: '30px' }}
+        //                             type="submit"
+        //                             disabled={isSaving}
+        //                         >
+        //                             Guardar
+        //                         </Button>
+        //                     </Box>
+        //                 </Grid>
+        //             </Grid>
+        //         </form>
+        //     </Box >
 
     )
 }
