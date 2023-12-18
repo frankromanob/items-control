da9a102a743d7ed746f8645902b1a786f75c9d23
Frank Romano <romano.fr@gmail.com>
2023-12-18 11:54:42 -0400

    Mongo fix

--- src/database/db.ts
+++ src/database/db.ts
@@ -17,16 +17,16 @@ export const connect = async () => {
     //     return;
     // }
 
-    // if (mongoose.connections.length > 0) {
-    //     mongoConnection.isConnected = mongoose.connections[0].readyState;
+    if (mongoose.connections.length > 0) {
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
 
-    //     if (mongoConnection.isConnected === 1) {
-    //         //console.log('Usando conexión anterior');
-    //         return;
-    //     }
+        if (mongoConnection.isConnected === 1) {
+            //console.log('Usando conexión anterior');
+            return;
+        }
 
-    //     await mongoose.disconnect();
-    // }
+        await mongoose.disconnect();
+    }
 
     await mongoose.connect(process.env.MONGO_URL || '');
 
